7cd8287612300f480560a142289528ad144dd284
Ana <anapaulacastro966@gmail.com>
2025-04-17 20:14:18 -0300

    commit referente a segunda questão, criação do modelo Todo, a migração do modelo para o banco de dados, a criação da view TodoList e a importação dos recursos necessarios, a configuração das urls para o direcionamento para Todo_List, além disso foi criado a pasta templates.

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Todo
 
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
