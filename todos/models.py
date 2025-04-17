07184128df1fc13e0a7efa285bfb930292e7ac4a
Ana <anapaulacastro966@gmail.com>
2025-04-17 20:32:14 -0300

    resolução da parte final do projeto, criada a classe TodoCreateView, importado as funcionalidades necessarias, realizado a criação do todo_form, e as configurações de conexão urls. O programa funciona perfeitamente.

--- todos/models.py
+++ todos/models.py
@@ -5,4 +5,4 @@ class Todo(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(null=False, blank=False)
-    finished_at = models.DateField(null=False)
+    finished_at = models.DateField(auto_now_add=True, null=False)
