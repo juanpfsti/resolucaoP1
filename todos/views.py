643ea3baff33bd2d4722e5a7bb0002b58fd3771c
Diego <diegoapf97@gmail.com>
2025-04-24 21:04:01 -0300

    aula delete and update

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Todo
 from django.urls import reverse_lazy
 
@@ -10,4 +10,13 @@ class TodoListView(ListView):
 class TodoCreateView(CreateView):
     model = Todo
     fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = Todo
     success_url = reverse_lazy('todo_list')
